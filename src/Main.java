8f474470331e2c4d17b7ae428eedfaec420506c5
ACER <snazarova0@>
2022-09-20 17:02:21 +0300

    Задание5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
+        var frog = 3.5;
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog / 3.5 + 4;
+        System.out.println(frog);
+
     }
 }
