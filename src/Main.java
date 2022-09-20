404eaa01eb989343d43da61aee5b13e5a599ee8e
ACER <snazarova0@>
2022-09-20 15:56:35 +0300

    Задание2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         var dog = 8;
+        dog = dog + 4;
         System.out.println(dog);
         var cat = 3.6;
+        cat = cat + 4;
         System.out.println(cat);
         var paper = 763789;
+        paper = paper + 4;
         System.out.println(paper);
     }
 }
